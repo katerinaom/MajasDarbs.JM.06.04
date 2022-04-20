f041234a441017c1796de38f05e3ce1cf740deb3
JekaterinaM <katerinaom@inbox.lv>
2022-04-20 13:54:08 +0300

    MajasDarbs2JM20.04

--- MajasDarbs2JM13.04/MajasDarbs2JM13.04/Program.cs
+++ MajasDarbs2JM13.04/MajasDarbs2JM13.04/Program.cs
@@ -1,10 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, my name is Jekaterina!");
-Console.WriteLine("What is your name?");
+Console.WriteLine("Hello, my name is Jekaterina! ");
+Console.WriteLine("What is your name? ");
 string userName = Console.ReadLine();
-string userGreeting = "Hello," + userName + "!";
+string userGreeting = "Hello, " + userName + "!";
 Console.WriteLine(userGreeting);
-Console.WriteLine("How old are you?");
+Console.WriteLine("How old are you? ");
 string userNumberText = Console.ReadLine();
 int userNumber = int.Parse(userNumberText);
 int result = userNumber + 1;
@@ -49,34 +49,42 @@ int tenNumber = int.Parse(Console.ReadLine());
 int leftafter = tenNumber % hundredNumber;
 Console.WriteLine("So, from hundred, ten is " + leftafter);
 
-Console.WriteLine("Now i will need your help in my next course task");
+Console.WriteLine("Now i will need your help in my next course task ");
 
-Console.WriteLine("Please, write a number");
+Console.WriteLine("Please, write a number ");
 int number = int.Parse(Console.ReadLine());
 bool isEven = number % 2 == 0;
 Console.WriteLine("Number is pair number: " + isEven);
 
 Console.WriteLine("Can you please help me a bit with math. I need your help with calculating the area of a rectangle? ");
 
-Console.WriteLine("like before, please write a number");
+Console.WriteLine("Like before, please write a number ");
 int firstRectangleNumber = int.Parse(Console.ReadLine());
-Console.WriteLine("And a second number");
+Console.WriteLine("And a second number ");
 int secondRectangleNumber = int.Parse(Console.ReadLine());
 int area = firstRectangleNumber * secondRectangleNumber;
-Console.WriteLine("the area of a rectangle is " + area);
-Console.WriteLine("Now we have an answer! Thank you!");
-Console.WriteLine("And tere are left only 2 questions, now i need to find the area of a right triangle");
-Console.WriteLine("Please, write a number of first side of triangle");
+Console.WriteLine("The area of a rectangle is " + area);
+Console.WriteLine("Now we have an answer! Thank you! ");
+Console.WriteLine("And tere are left only 2 questions, now i need to find the area of a right triangle ");
+Console.WriteLine("Please, write a number of first side of triangle ");
 int triangleFirstNumber = int.Parse(Console.ReadLine());
 
-Console.WriteLine("Please, write a number of second side of triangle");
+Console.WriteLine("Please, write a number of second side of triangle ");
 
 int triangleSecondNumber = int.Parse(Console.ReadLine());
 
-int Max = (triangleFirstNumber + triangleSecondNumber) / 2;
+double Max = (triangleFirstNumber + (double) triangleSecondNumber) / 2;
 
-Console.WriteLine("the two sides of a right triangle is " + Max);
- 
+Console.WriteLine("The area of a right triangle is " + (double) Max);
+Console.WriteLine("And the last one! ");
+
+Console.WriteLine("Please, write your name! ");
+string userName2 = Console.ReadLine();
+Console.WriteLine("Please, write your age! ");
+int userAge = int.Parse(Console.ReadLine());
+Console.WriteLine($"Hello {userName2}, your age is {userAge}.");
+
+
 
 
 
